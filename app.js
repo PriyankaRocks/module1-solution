dc17e1829ea70d15bf2b47d556569ee68fc39dcf
Priyanka Kumari <priyanka.kumari@wavemaker.com>
2018-08-18 13:28:55 +0530

    completed assignment 1

--- app.js
+++ app.js
@@ -2,18 +2,15 @@
 'use strict';
 
 angular.module('LunchCheck', [])
-.controller('MsgController', MsgController);
-
-MsgController.$inject = ['$scope'];
-function MsgController($scope) {
-  $scope.name = "Priyanka";
-  $scope.sayMessage = function() {
-    return " loves to play badminton!";
-  }
-  $scope.stateofbeing = "happy";
-  $scope.feedPriyanka = function() {
-    $scope.stateofbeing = "disgust";
-  };
-}
+.controller('LunchCheckController', function ($scope) {
+    $scope.name = "Priyanka!";
+    $scope.checkItemCounts = function() {
+      let inputString = ($scope.name).split(",");
+      let count = inputString.filter(function(val) {
+        return val.trim() != "";
+      }).length;
+      $scope.displayMessage = (count > 3) ? "Too much!": "Enjoy!";
+    };
+});
 
 })();
